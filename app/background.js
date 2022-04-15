3e1b12ad95ac5c573392ab2bcc6253d49df0c5be
Aditya Khatri <adityakhatri47@gmail.com>
2022-04-15 16:59:00 +0545

    Migrate to manifest v3

--- app/background.js
+++ app/background.js
@@ -3,7 +3,7 @@ const EXTENSION_GET_SCREENSHOT_MESSAGE = 'deep-get-screenshot';
 chrome.runtime.onMessageExternal.addListener((request, _, reply) => {
     switch (request.message) {
         case EXTENSION_GET_SCREENSHOT_MESSAGE: {
-            chrome.tabs.captureVisibleTab(null, {}, (image) => {
+            chrome.tabs.captureVisibleTab(undefined, {}, (image) => {
                 reply({
                     message: 'success',
                     image,
